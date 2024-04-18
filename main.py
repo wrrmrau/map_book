4e94b086eb4f9fef300c665984048064934c8c48
wrrmrau <166690607+wrrmrau@users.noreply.github.com>
2024-04-18 10:50:21 +0200

    wip: refactoring to folders

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from models.data import users
-from utils.crud import show_users
-
+from utils.crud import show_users, add_new_user
 
 
 
@@ -8,11 +7,15 @@ from utils.crud import show_users
 if __name__ == "__main__":
     print("witaj użytkowniku"),
     while True:
-        print("Menu:"),
+        print("Menu:")
+        print("0. zakończ program")
         print("1. wyświetl co u znajomych")
+        print("2. dodaj użytkownika")
         menu_option: str = input("Dokonaj wyboru:")
         if menu_option == "0":
             print("program kończy pracę")
             break
         if menu_option == "1":
             show_users(users)
+        if menu_option == "2":
+            add_new_user(users)
--- utils/crud.py
+++ utils/crud.py
@@ -2,3 +2,9 @@ def show_users(user_list: list[dict]) -> None:
     for user in user_list:
         print(f"Twój znajomy {user['name']} opublikował: {user['posts']} postów")
 
+def add_new_user(users: list)-> None:
+    new_name: str = input("Enter your new name: ")
+    new_surname: str = input("Enter your new surname: ")
+    new_posts: int = int(input("Enter your new posts: "))
+    new_user: dict = {"name": new_name, "surname": new_surname, "posts": new_posts}
+    users.append(new_user)
