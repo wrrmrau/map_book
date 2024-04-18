db54899bec51a592a9da6b05813f01d8da361caa
wrrmrau <166690607+wrrmrau@users.noreply.github.com>
2024-04-18 08:44:27 +0200

    wip: add user printing functionality

---
+++ main.py
@@ -0,0 +1,18 @@
+imie="Dawid"
+nazwisko="Bałuka"
+liczba_postow=6000
+
+print(f"Twój znajomy {imie} {nazwisko} opublikował {liczba_postow} postów")
+
+imie_2="Kewin"
+nazwisko_2="Czajkowski"
+liczba_postow_2=6002
+
+print(f"Twój znajomy {imie_2} {nazwisko_2} opublikował {liczba_postow_2} postów")
+
+imie_3="Kamil"
+nazwisko_3="Gil"
+liczba_postow_3=1_000_000
+
+print(f"Twój znajomy {imie_3} {nazwisko_3} opublikował {liczba_postow_3} postów")
+
