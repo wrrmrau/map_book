2faadac5902f9d5aa74aa02035df1c0a9a4cc5fb
wrrmrau <166690607+wrrmrau@users.noreply.github.com>
2024-04-18 11:24:03 +0200

    wip: add remove user funcion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from models.data import users
-from utils.crud import show_users, add_new_user
+from utils.crud import show_users, add_new_user, search_user, remove_user
 
 
 
@@ -11,6 +11,8 @@ if __name__ == "__main__":
         print("0. zakończ program")
         print("1. wyświetl co u znajomych")
         print("2. dodaj użytkownika")
+        print("3. znajdź użytkownika")
+        print("4. usuń użytkownika")
         menu_option: str = input("Dokonaj wyboru:")
         if menu_option == "0":
             print("program kończy pracę")
@@ -19,3 +21,7 @@ if __name__ == "__main__":
             show_users(users)
         if menu_option == "2":
             add_new_user(users)
+        if menu_option == "3":
+            search_user(users)
+        if menu_option == "4":
+            remove_user(users)
--- utils/crud.py
+++ utils/crud.py
@@ -8,3 +8,15 @@ def add_new_user(users: list)-> None:
     new_posts: int = int(input("Enter your new posts: "))
     new_user: dict = {"name": new_name, "surname": new_surname, "posts": new_posts}
     users.append(new_user)
+
+def search_user(users) -> None:
+    kogo_szukasz = input("kogo szukasz: ")
+    for user in users:
+        if user["name"] == kogo_szukasz:
+            print(user)
+
+def remove_user(users) -> None:
+    kogo_szukasz = input("kogo szukasz: ")
+    for user in users:
+        if user["name"] == kogo_szukasz:
+            users.remove(user)
